80af3e346e7574aebdd7c23e258309b1f864b27e
macOs <max.kriv3228@gmail.com>
2024-03-04 19:19:42 +0300

    first commit

---
+++ server/index.js
@@ -0,0 +1,25 @@
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+const errorHandler = require('./middlewares/errorHandlerMiddleware');
+
+const PORT = process.env.PORT || 5000;
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.use(errorHandler);
+
+const start = async () => {
+  try {
+    app.get('/', (req, res) => {
+      res.send('Hello World!');
+    });
+    app.listen(PORT, () => console.log(`Server started at port: ${PORT}`));
+  } catch (err) {
+    console.log(err.message);
+  }
+};
+
+start();
