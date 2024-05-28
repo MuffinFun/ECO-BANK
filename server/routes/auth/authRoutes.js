15075089da22c2560370777fea133a6231925df9
macOs <max.kriv3228@gmail.com>
2024-05-29 01:12:42 +0300

    create main auth functions

--- server/routes/auth/authRoutes.js
+++ server/routes/auth/authRoutes.js
@@ -1,8 +1,14 @@
 const Router = require('express');
 const router = new Router();
 const authController = require('../../controllers/auth/authController');
+const { body } = require('express-validator');
 
-router.post('/registration', authController.registration);
+router.post(
+  '/registration',
+  body('email').isEmail(),
+  body('password').isLength({ min: 6, max: 24 }),
+  authController.registration
+);
 router.post('/login', authController.login);
 router.post('/logout', authController.logout);
 router.get('/activate/:link', authController.activate);
--- server/service/auth-services/userService.js
+++ server/service/auth-services/userService.js
@@ -8,7 +8,7 @@ const bcrypt = require('bcrypt');
 const uuid = require('uuid');
 
 class UserService {
-  async registration(login, email, password) {
+  async registration(login, email, password, role) {
     try {
       const emailCandidate = await UserAccount.findOne({ where: { email } });
       const loginCandidate = await UserInfo.findOne({ where: { login } });
@@ -17,10 +17,6 @@ class UserService {
         throw ApiError.badRequest(`User with email - ${email}, already exist!`);
       } else if (loginCandidate) {
         throw ApiError.badRequest(`This login already exist!`);
-      }
-
-      if (password.length < 7 || password.length > 20) {
-        throw ApiError.badRequest('password length must be in 7-20 letters');
       }
 
       const hashPassword = await bcrypt.hash(password, 3);
@@ -38,7 +34,7 @@ class UserService {
         activation_link: activationLink,
       });
 
-      const userDto = new UserDto(user, email);
+      const userDto = new UserDto(user, email, role);
       const tokens = tokenService.generateTokens({ ...userDto });
 
       await tokenService.saveToken(userDto.userId, tokens.refreshToken);
@@ -61,6 +57,54 @@ class UserService {
     } catch (error) {
       throw ApiError.badRequest(error.message);
     }
+  }
+  async login(login, password, email, role) {
+    const user = await UserInfo.findOne({ where: { login } });
+    if (!user) {
+      throw ApiError.badRequest('user with Login not found');
+    }
+
+    const isPassEquals = await bcrypt.compare(password, user.password);
+    if (!isPassEquals) {
+      throw ApiError.badRequest('incorrect password');
+    }
+    const userDto = new UserDto(user, email, role.toUpperCase());
+
+    const tokens = tokenService.generateTokens({ ...userDto });
+
+    await tokenService.saveToken(userDto.userId, tokens.refreshToken);
+
+    return { ...tokens, userInfo: userDto };
+  }
+  async logout(refreshToken) {
+    const token = await tokenService.removeToken(refreshToken);
+    if (token !== 1) {
+      throw ApiError.badRequest('something went wrong with logout');
+    }
+    return token;
+  }
+  async refresh(refreshToken) {
+    if (!refreshToken) {
+      throw ApiError.UnauthorizedError();
+    }
+    const userData = tokenService.validateRefreshToken(refreshToken);
+    const tokenFromDb = await tokenService.findToken(refreshToken);
+    if (!userData || !tokenFromDb) {
+      throw ApiError.UnauthorizedError();
+    }
+
+    const user = await UserInfo.findByPk(userData.userId);
+    const userDto = new UserDto(
+      user,
+      userData.email,
+      userData.role.toUpperCase()
+    );
+
+    const tokens = tokenService.generateTokens({ ...userDto });
+
+    await tokenService.saveToken(userDto.userId, tokens.refreshToken);
+
+    return { ...tokens, userInfo: userDto };
   }
 }
 
